fa1a71da66053aac6070f8a118ad97f127786357
Andrey <chupic_ekb@mail.ru>
2021-01-28 10:16:57 +0500

    git

---
+++ functions.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let num = 20;
+
+function showFirstmassege (text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstmassege("hello world");
+console.log(num);
+
+
+function calc (a, b) {
+    return (a+b);
+}
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret () {
+    let num = 50;
+    return(num);
+}
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function () {
+    console.log("Hello")
+};
+logger();
+
+const sum = (a, b) => {return a+b};
