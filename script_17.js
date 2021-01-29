96e7a6a97b5b609905c78be406485492cb51be42
Andrey <chupic_ekb@mail.ru>
2021-01-29 09:51:20 +0500

    lesson 17

---
+++ script_17.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const str = "test";
+const arr = [1, 2, 3];
+
+console.log (str.toUpperCase());
+
+console.log (str);
+console.log (str[2]);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("q"));
